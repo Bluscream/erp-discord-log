72d575702066247298768a0a5810d31a689f4f2e
Bluscream <Bluscream@github.io>
2021-05-27 21:58:47 +0200

    fixing playerlog hopefully uwu
    Signed-off-by: Bluscream <Bluscream@github.io>

--- main.py
+++ main.py
@@ -18,26 +18,18 @@ from Classes.ServerResponseSingle import server_response_single_from_dict, Serve
 def modification_date(filename):
     t = path.getmtime(filename)
     return datetime.fromtimestamp(t)
-
-
 def file_age_in_seconds(pathname):
     return time() - os_stat(pathname)[ST_MTIME]
-
-
 def embed_not_empty(embed):
     return embed.title or embed.description or (embed.footer is not discord.Embed.Empty) or (
                 embed.fields is not discord.Embed.Empty)
-
-
 def cacheFile(id):
     return f"cache/{id}.cache.json"
-
-
 def sanitize(input):
     return re.sub(r"\^\d", "", input.strip(), 0, re.MULTILINE)
 
 
-def getDiffText(old, new):
+def getDiff(old, new):
     missing = (set(old).difference(new));
     is_missing = len(missing) > 0
     added = (set(new).difference(old));
@@ -46,6 +38,20 @@ def getDiffText(old, new):
         i = ["```diff"]
         if is_missing: i.append("- " + '\n- '.join(missing))
         if is_added:   i.append("+ " + '\n+ '.join(added))
+        i.append("```")
+        return '\n'.join(i)
+    return None
+
+
+def getPlayerDiff(old, new):
+    missing = (set(old).difference(new))
+    is_missing = len(missing) > 0
+    added = (set(new).difference(old))
+    is_added = len(added) > 0
+    if is_missing or is_added:
+        i = ["```diff"]
+        if is_missing: i.append("- " + '\n- '.join(getPlayers(missing)))
+        if is_added:   i.append("+ " + '\n+ '.join(getPlayers(added)))
         i.append("```")
         return '\n'.join(i)
     return None
@@ -118,15 +124,15 @@ class MyClient(discord.Client):
                 # CHANGES START
                 if fivem_server.data.resources != last_response.data.resources:
                     embed.add_field(name="Resources",
-                                    value=getDiffText(last_response.data.resources, fivem_server.data.resources));
+                                    value=getDiff(last_response.data.resources, fivem_server.data.resources));
                     changes += 1
                 if fivem_server.data.vars.sv_enforce_game_build != last_response.data.vars.sv_enforce_game_build:
                     embed.add_field(name="Game Version",
                                     value=f"```diff\n-{last_response.data.vars.sv_enforce_game_build}\n+{fivem_server.data.vars.sv_enforce_game_build}```");
                     changes += 1
                 if fivem_server.data.players != last_response.data.players:
-                    embed.add_field(name="Players", value=getDiffText(getPlayers(last_response.data.players),
-                                                                      getPlayers(fivem_server.data.players)),
+                    embed.add_field(name="Players",
+                                    value=getPlayerDiff(last_response.data.players, fivem_server.data.players),
                                     inline=False);
                     changes += 1
                 # CHANGES END
